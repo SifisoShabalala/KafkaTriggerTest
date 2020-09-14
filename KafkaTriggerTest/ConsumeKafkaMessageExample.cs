7e6d948b88e100ff9e2139631db42503991b8638
Sifiso Shabalala <sifiso.shabalala10@gmail.com>
2020-09-15 00:03:50 +0200

    Add project files.

---
+++ KafkaTriggerTest/ConsumeKafkaMessageExample.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.WebJobs.Extensions.Kafka;
+
+namespace KafkaTriggerTest
+{
+    public static class ConsumeKafkaMessageExample
+    {
+        [FunctionName("ConsumeKafkaMessage")]
+        public static void ConsumeKafkaMessage(
+            [KafkaTrigger("kafka-26b55b43-sifisoshabalala173-36cd.aivencloud.com:27704",
+            "EVT_GITHUB_COMMITS",
+            Protocol = BrokerProtocol.SaslSsl, 
+            AuthenticationMode = BrokerAuthenticationMode.Plain,
+            ConsumerGroup = "KAFKA_CONSUMER_EXAMPLE",
+            Username = "avnadmin",
+            SslCaLocation = "ca.pem",
+            SslCertificateLocation = "service.cert",
+            SslKeyLocation = "service.key",
+            Password = "v84gp12ojyz8brnp")] KafkaEventData<string> message,
+            ILogger log)
+        {
+            log.LogInformation(message.Value);
+        }
+    }
+}
---
+++ KafkaTriggerTest/Message.cs
@@ -0,0 +1,9 @@
+﻿namespace KafkaTriggerTest
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
